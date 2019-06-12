429fbd7a0af68d7fbdb534140e08f83df27f5f75
Venkatalakshmi <49299119+VenkatalakshmiM@users.noreply.github.com>
2019-06-12 10:37:01 +0530

    Create vowels.py

---
+++ my programs/vowels.py
@@ -0,0 +1,8 @@
+inputcharacter=input()
+if(inputcharacter.isalpha()):
+  if(inputcharacter=="a" or inputcharacter=="e" or inputcharacter=="i" or inputcharacter=="o" or inputcharacter=="u"):
+    print("Vowel")
+  else:
+    print("Consonant")
+else:
+   print("invalid")
