b5513d37cf0a35229d57c83107837a1e89a6e2c4
Venkatalakshmi <49299119+VenkatalakshmiM@users.noreply.github.com>
2019-06-12 17:02:24 +0530

    Update evenInterval.py

--- my programs/evenInterval.py
+++ my programs/evenInterval.py
@@ -1,8 +1,6 @@
-number1=int(input())
-number2=int(input())
-
-for i in range(number1,number2):
-  if(i==number1 or i==number2 or (i%2!=0)):
-    continue
-  else:
-    print(i)
+x=input().split()
+number1=int(x[0])
+number2=int(x[1])
+for i in range(number2):
+ if(i>1 and i%2==0):
+   print(i,end=" ")
