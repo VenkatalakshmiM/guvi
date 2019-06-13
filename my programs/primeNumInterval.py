ba6ef155c54d50baf7e68c717130610c087ba403
Venkatalakshmi <49299119+VenkatalakshmiM@users.noreply.github.com>
2019-06-13 10:35:00 +0530

    Update primeNumInterval.py

--- my programs/primeNumInterval.py
+++ my programs/primeNumInterval.py
@@ -1,9 +1,10 @@
 num1=int(input())
 num2=int(input())
 for k in range(num1,num2+1):
-  if k>1:
+  if k>2:
     for i in range(2,k):
-      if(k%i==0 or k==num1 or k==num2):
+      if(k%i==0):
         break
     else:
-      print(k)
+      if(k!=num1 or k!=num2):
+        print(k)
