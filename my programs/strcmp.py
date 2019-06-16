b6c0caa44d565030a9e4b6b1835cc21cf7d803e7
Venkatalakshmi <49299119+VenkatalakshmiM@users.noreply.github.com>
2019-06-16 19:13:00 +0530

    Update strcmp.py

--- my programs/strcmp.py
+++ my programs/strcmp.py
@@ -1,13 +1,5 @@
 str1,str2=input().split()
-count1=0
-count2=0
-for i in str1:
-  count1=count1+1
-for j in str2:
-  count2=count2+1
-if(count1<count2):
-  print(str2)
-elif(count1==count2):
+if str1>=str2:
   print(str1)
 else:
-  print(str1)
+  print(str2)
