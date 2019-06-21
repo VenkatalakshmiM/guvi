67381166659f00cc8ae2b9cf2e28d14fa0c03a5f
Venkatalakshmi <49299119+VenkatalakshmiM@users.noreply.github.com>
2019-06-21 16:14:06 +0530

    Create vowel.py

---
+++ my programs/vowel.py
@@ -0,0 +1,7 @@
+strng=input()
+for i in strng:
+  if(i=='a' or i=='e' or i=='i' or i=='o' or i=='u' or i=='A' or i=='E' or i=='I' or i=='O' or i=='U'):
+    print("yes")
+    break
+else:
+    print("no")
