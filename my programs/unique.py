56fcdd86cc894433061b87495652edc62ed3c0ff
Venkatalakshmi <49299119+VenkatalakshmiM@users.noreply.github.com>
2019-06-26 16:17:51 +0530

    Create unique.py

---
+++ my programs/unique.py
@@ -0,0 +1,15 @@
+n1=int(input())
+lst=list(map(int,input().split()))
+u=[]
+for i in range(n1):
+  for j in range(i+1,len(lst)):
+    if(lst[j]==lst[i]):
+      u.append(lst[i])
+
+if len(u)==0:
+  print("unique")
+else:
+  u.sort()
+  k=set(u)
+  for i in k:
+    print(k,end=" ")
